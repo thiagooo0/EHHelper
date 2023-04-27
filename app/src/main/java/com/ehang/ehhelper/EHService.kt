e0a9b247115e227e789362ea61dad63d41981cf7
guoshaohong <shoahong.guo@ehang.com>
2023-04-27 18:44:24 +0800

    use AccessibilityService to capture KEYCODE_DPAD_UP,KEYCODE_DPAD_DOWN in Douyin and change it to scroll motion

--- app/src/main/java/com/ehang/ehhelper/EHService.kt
+++ app/src/main/java/com/ehang/ehhelper/EHService.kt
@@ -1,24 +1,31 @@
 package com.ehang.ehhelper
 
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.accessibilityservice.GestureDescription.StrokeDescription
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.Service
 import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.Path
 import android.graphics.PixelFormat
+import android.graphics.PointF
 import android.os.Build
-import android.os.IBinder
+import android.text.TextUtils
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.Gravity
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.view.WindowManager.LayoutParams
+import android.view.accessibility.AccessibilityEvent
 import androidx.core.app.NotificationCompat
 
 
-class EHService: Service() {
+class EHService: AccessibilityService() {
 	private val TAG = "EHService"
 	private val NOTIFICATION_ID = 1
 	private val name = "Ehang Helper"
@@ -29,6 +36,10 @@ class EHService: Service() {
 	private var initialY: Int = 0
 	private var initialTouchX: Float = 0.toFloat()
 	private var initialTouchY: Float = 0.toFloat()
+
+	private lateinit var upPath: Path
+	private lateinit var downPath: Path
+
 
 	private val windowManager: WindowManager by lazy { getSystemService(WINDOW_SERVICE) as WindowManager }
 	private val floatingButton: View by lazy {
@@ -67,6 +78,8 @@ class EHService: Service() {
 		params
 	}
 
+	private val douyinPackageName = "com.ss.android.ugc.aweme"
+	private var currentPackageName = ""
 
 	override fun onCreate() {
 		super.onCreate()
@@ -81,11 +94,29 @@ class EHService: Service() {
 		startForeground(NOTIFICATION_ID, builder.build())
 
 		windowManager.addView(floatingButton, fbParams)
-	}
 
-	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-		Log.d(TAG, "onStartCommand")
-		return super.onStartCommand(intent, flags, startId)
+		val displayMetrics = DisplayMetrics()
+		windowManager.defaultDisplay.getMetrics(displayMetrics)
+		//douyin definitely open in Portrait mode
+		var screenWidth = 0
+		var screenHeight = 0
+		if(displayMetrics.widthPixels < displayMetrics.heightPixels) {
+			screenWidth = displayMetrics.widthPixels
+			screenHeight = displayMetrics.heightPixels
+		} else {
+			screenWidth = displayMetrics.heightPixels
+			screenHeight = displayMetrics.widthPixels
+		}
+		val startPoint = PointF(screenWidth / 2f, screenHeight / 4f)
+		val endPoint = PointF(screenWidth / 2f, screenHeight * 3 / 4f)
+		Log.d(TAG, "startPoint $startPoint")
+		Log.d(TAG, "endPoint $endPoint")
+		upPath = Path()
+		upPath.moveTo(startPoint.x, startPoint.y)
+		upPath.lineTo(endPoint.x, endPoint.y)
+		downPath = Path()
+		downPath.moveTo(endPoint.x, endPoint.y)
+		downPath.lineTo(startPoint.x, startPoint.y)
 	}
 
 	override fun onDestroy() {
@@ -93,8 +124,44 @@ class EHService: Service() {
 		windowManager.removeView(floatingButton)
 	}
 
-	override fun onBind(intent: Intent): IBinder? {
-		return null
+	override fun onKeyEvent(event: KeyEvent): Boolean {
+		if(TextUtils.equals(currentPackageName, douyinPackageName)){
+			when(event.keyCode) {
+				KeyEvent.KEYCODE_DPAD_UP -> {
+					if(event.action == KeyEvent.ACTION_DOWN) {
+						Log.d(TAG, "往上滑")
+						dispatchGesture(
+							GestureDescription.Builder().addStroke(StrokeDescription(upPath, 0, 200)).build(),
+							null, null)
+					}
+					return true
+				}
+
+				KeyEvent.KEYCODE_DPAD_DOWN -> {
+					if(event.action == KeyEvent.ACTION_DOWN) {
+						Log.d(TAG, "往下滑")
+						dispatchGesture(
+							GestureDescription.Builder().addStroke(StrokeDescription(downPath, 0, 200)).build(),
+							null, null)
+					}
+					return true
+				}
+			}
+		}
+		return super.onKeyEvent(event)
+	}
+
+	override fun onAccessibilityEvent(event: AccessibilityEvent) {
+		if(event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+			val nodeInfo = event.source
+			if(nodeInfo != null) {
+				currentPackageName = nodeInfo.packageName.toString()
+				Log.d(TAG, "Current package name: $currentPackageName")
+			}
+		}
+	}
+
+	override fun onInterrupt() {
 	}
 
 	private fun createNotificationChannel() {
